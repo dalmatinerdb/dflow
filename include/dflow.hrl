886f7db45ddc7f7c1b7ca624c9b4a0434674e2c9
David Cromberge <david.cromberge@dataloop.io>
2017-01-24 15:35:41 +0000

    Add elapsed time to digraph edges

--- include/dflow.hrl
+++ include/dflow.hrl
@@ -1,8 +1,14 @@
+-record(timing_info, {
+          start :: integer(),
+          stop :: integer() }).
+
 -record(node, {
           pid :: pid(),
           desc :: iodata(),
           in :: pos_integer(),
           out :: pos_integer(),
           done :: boolean(),
+          timing :: #timing_info{},
           children :: [#node{}]
          }).
+
